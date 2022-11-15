2a1e7791b9f65634c3d24d0e7477e898d528c162
tt-7495 <hitrov.95@mail.ru>
2022-11-15 22:51:29 +0500

    first commit

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,7 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public abstract class Animal {
 }
